a4dab7016f9cd3dc2468ea587a8e834700c1a4ca
Rob Looby <robertjlooby@gmail.com>
2017-06-30 20:49:22 -0500

    Add logging

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,13 @@
 module Main where
 
 import App (app)
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import System.Environment (getEnv)
 import qualified Web.Scotty as S
 
 main :: IO ()
 main = do
   port <- read <$> getEnv "PORT"
-  S.scotty port app
+  S.scotty port $ do
+    S.middleware logStdout
+    app
