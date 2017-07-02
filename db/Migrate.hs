2cafc534863e54553171be4e4a91e7c86b88a26a
Rob Looby <robertjlooby@gmail.com>
2017-07-02 13:13:49 -0500

    Add migrate executable and migration for projects table

---
+++ db/Migrate.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import qualified Data.ByteString.Char8 as BS8
+import Database.PostgreSQL.Simple ( connectPostgreSQL, withTransaction)
+import Database.PostgreSQL.Simple.Migration (MigrationCommand(..), MigrationContext(..), MigrationResult, runMigration)
+import System.Environment (getEnv)
+
+main :: IO (MigrationResult String)
+main = do
+  dbUrl <- BS8.pack <$> getEnv "DATABASE_URL"
+  conn <- connectPostgreSQL dbUrl
+  withTransaction conn $ do
+    _ <- runMigration $ MigrationContext MigrationInitialization True conn
+    runMigration $ MigrationContext (MigrationDirectory "db/migrations") True conn
