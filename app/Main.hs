5d3b93c30bd9e4224ecb67bf0100c40d1d4069ac
Rob Looby <robertjlooby@gmail.com>
2017-07-07 14:03:46 -0500

    Force HTTPS in production environment

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,25 @@
 module Main where
 
 import App (app)
+import Network.Wai.Middleware.ForceSSL (forceSSL)
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import Network.Wai.Middleware.Static (addBase, staticPolicy)
-import System.Environment (getEnv)
+import System.Environment (getEnv, lookupEnv)
 import qualified Web.Scotty as S
+
+getSSLMiddleware :: IO (S.ScottyM ())
+getSSLMiddleware = do
+  environment <- lookupEnv "APP_ENV"
+  if environment == Just "production"
+     then return $ S.middleware forceSSL
+     else return $ return ()
 
 main :: IO ()
 main = do
     port <- read <$> getEnv "PORT"
+    sslMiddleware <- getSSLMiddleware
     S.scotty port $ do
         S.middleware logStdout
+        sslMiddleware
         S.middleware $ staticPolicy (addBase "app/static")
         app
