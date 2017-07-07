1b62f2b68d93a68101c02b510f09c91832138479
Rob Looby <robertjlooby@gmail.com>
2017-07-06 23:41:13 -0500

    Add Project with simple create/find

---
+++ app/models/Project.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Project where
+
+import Data.Text (Text)
+import Database.PostgreSQL.Simple (Connection, FromRow, Only(..), query)
+import Database.PostgreSQL.Simple.FromRow (field, fromRow)
+
+newtype ProjectId = ProjectId Int deriving (Eq, Ord, Show)
+
+instance FromRow ProjectId where
+  fromRow = ProjectId <$> field
+
+data Project = Project
+    { name :: Text
+    , description :: Text
+    } deriving (Eq, Show)
+
+create :: Connection -> Text -> Text -> IO ProjectId
+create conn name description = do
+  ids <- query conn "INSERT INTO projects (name, description) VALUES (?, ?) RETURNING id" (name, description)
+  case ids of
+    [(Only projectId)] -> return $ ProjectId projectId
+
+find :: Connection -> ProjectId -> IO (Maybe Project)
+find conn (ProjectId projectId) = do
+  project <- query conn "SELECT name, description FROM projects WHERE id = ?" $ Only projectId
+  case project of
+    [(name, description)] -> return $ Just $ Project name description
+    _ -> return Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,8 +2,18 @@
 
 module Main where
 
+import qualified Data.ByteString.Char8 as BS8
+import Database.PostgreSQL.Simple (connectPostgreSQL)
+import Database.PostgreSQL.Simple.Util (withTransactionRolledBack)
+import Test.Hspec (Spec, around_, hspec)
+import System.Environment (getEnv)
+
 import qualified AppSpec
-import Test.Hspec (Spec, describe, hspec, it)
+import qualified ProjectSpec
 
 main :: IO ()
-main = hspec AppSpec.spec
+main = do
+    conn <- BS8.pack <$> getEnv "DATABASE_URL" >>= connectPostgreSQL
+    hspec $ around_ (withTransactionRolledBack conn) $ do
+        AppSpec.spec
+        ProjectSpec.spec conn
