0e4efa8eae65e196a01d664f29ccba8a7286c6e1
Rob Looby <robertjlooby@gmail.com>
2017-07-08 13:40:31 -0500

    Add Projects index page

--- app/App.hs
+++ app/App.hs
@@ -4,6 +4,7 @@ module App where
 
 import Database.PostgreSQL.Simple (Connection)
 import qualified Views.Index
+import qualified Views.Project
 import qualified Project as P
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Web.Scotty as S
@@ -12,6 +13,9 @@ app :: Connection -> S.ScottyM ()
 app conn = do
     S.get "/" $ do
         S.html $ renderHtml Views.Index.index
+    S.get "/projects" $ do
+        projects <- S.liftAndCatchIO $ P.findAll conn
+        S.html $ renderHtml $ Views.Project.index projects
     S.post "/projects" $ do
         name <- S.param "name"
         description <- S.param "description"
--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -14,6 +14,9 @@ spec conn = with (S.scottyApp $ app conn) $ do
     describe "GET /" $ do
         it "responds with 200" $ do
             get "/" `shouldRespondWith` 200
+    describe "GET /projects" $ do
+        it "responds with a 200" $ do
+            get "/projects" `shouldRespondWith` 200
     describe "POST /projects" $ do
         it "responds with a 302" $ do
             postHtmlForm "/projects" [("name", "project"), ("description", "the project")] `shouldRespondWith` 302
