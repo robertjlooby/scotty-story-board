36e2c2901468f2149e838e1f19da5b32d4c8eade
Rob Looby <robertjlooby@gmail.com>
2017-07-15 14:20:00 -0500

    wip: using wai-middleware-auth

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,12 @@ module Main where
 
 import App (app)
 import qualified Data.ByteString.Char8 as BS8
+import Data.HashMap.Lazy (fromList)
 import Database.PostgreSQL.Simple (connectPostgreSQL)
+import Network.Wai.Middleware.Auth
+import Network.Wai.Middleware.Auth.OAuth2.Github
+import Network.Wai.Middleware.Auth.OAuth2.Google
+import Network.Wai.Middleware.Auth.Provider
 import Network.Wai.Middleware.ForceSSL (forceSSL)
 import Network.Wai.Middleware.MethodOverridePost (methodOverridePost)
 import Network.Wai.Middleware.RequestLogger (logStdout)
@@ -24,9 +29,13 @@ main = do
     conn <- BS8.pack <$> getEnv "DATABASE_URL" >>= connectPostgreSQL
     port <- read <$> getEnv "PORT"
     sslMiddleware <- getSSLMiddleware
+    let github = mkGithubProvider "Scotty Story Board" "id" "secret" [] Nothing
+    let google = mkGoogleProvider "id" "secret" [] Nothing
+    authMiddleware <- mkAuthMiddleware (setAuthProviders (fromList [("Google", Provider google), ("GitHub", Provider github)]) defaultAuthSettings)
     S.scotty port $ do
         S.middleware methodOverridePost
         S.middleware logStdout
         sslMiddleware
         S.middleware $ staticPolicy (addBase "app/static")
+        S.middleware authMiddleware
         app conn
