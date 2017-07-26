ebf042dbd266ad358cf0d725b1a22ab5a33c7336
Rob Looby <robertjlooby@gmail.com>
2017-07-25 22:30:25 -0500

    Move session get/middleware to Session

--- app/Session.hs
+++ app/Session.hs
@@ -3,16 +3,18 @@
 
 module Session where
 
-import           Data.Aeson (FromJSON, ToJSON, encode, defaultOptions, genericToEncoding, toEncoding)
-import           Data.ByteString (ByteString)
+import           Data.Aeson (FromJSON, ToJSON, encode, decodeStrict, defaultOptions, genericToEncoding, toEncoding)
 import qualified Data.ByteString.Lazy as BSL
 import           Data.String (IsString)
-import qualified Data.Text.Encoding as E
+import qualified Data.Vault.Lazy as Vault
 import           GHC.Generics (Generic)
-import           Web.ClientSession (Key, decrypt, encryptIO)
-import           Web.Cookie (def, setCookieName, setCookiePath, setCookieValue)
+import           Network.HTTP.Types.Header (hCookie)
+import           Network.Wai (Middleware, requestHeaders, vault)
+import           Web.ClientSession (decrypt, encryptIO)
+import qualified Web.ClientSession as CS
+import           Web.Cookie (def, parseCookies, setCookieName, setCookiePath, setCookieValue)
 import qualified Web.Scotty as S
-import           Web.Scotty.Cookie (getCookie, setCookie)
+import           Web.Scotty.Cookie (setCookie)
 
 import qualified User
 import           User (User, UserId)
@@ -29,19 +31,30 @@ instance FromJSON Session
 sessionCookieName :: IsString a => a
 sessionCookieName = "session"
 
-setSessionCookie :: User -> Key -> S.ActionM ()
-setSessionCookie user key = do
+setSessionCookie :: User -> CS.Key -> S.ActionM ()
+setSessionCookie user sessionKey = do
     let session = Session (User.id_ user)
-    encrypted <- S.liftAndCatchIO $ encryptIO key (BSL.toStrict . encode $ session)
+    encrypted <- S.liftAndCatchIO $ encryptIO sessionKey (BSL.toStrict . encode $ session)
     let cookie = def { setCookieName = sessionCookieName
                      , setCookieValue = encrypted
                      , setCookiePath = Just "/"
                      }
     setCookie cookie
 
-getSessionCookie :: Key -> S.ActionM (Maybe ByteString)
-getSessionCookie key = do
-    cookie <- getCookie sessionCookieName
-    case cookie of
-      Just d -> return $ decrypt key (E.encodeUtf8 d)
-      Nothing -> return Nothing
+sessionMiddleware :: CS.Key -> Vault.Key Session -> Middleware
+sessionMiddleware sessionKey vaultKey app request =
+    case findSession of
+      Just session -> app $ request { vault = Vault.insert vaultKey session (vault request) }
+      Nothing -> app request
+  where
+    findSession :: Maybe Session
+    findSession = do
+        cookieHeader <- lookup hCookie $ requestHeaders request
+        sessionCookie <- lookup sessionCookieName $ parseCookies cookieHeader
+        session <- decrypt sessionKey sessionCookie
+        decodeStrict session
+
+getSession :: Vault.Key Session -> S.ActionM (Maybe Session)
+getSession vaultKey = do
+    request <- S.request
+    return $ Vault.lookup vaultKey (vault request)
