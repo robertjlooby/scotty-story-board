3f7e52b5a74d695cd17a83ed7fe1a03c28d3f542
Rob Looby <robertjlooby@gmail.com>
2017-08-05 21:53:48 -0500

    Add ToValue instance for Project

--- app/views/ProjectViews.hs
+++ app/views/ProjectViews.hs
@@ -19,12 +19,10 @@ index projects = Layouts.app $ do
                 p . (a ! href "/projects/new") $ "New"
   where
     showProject project = do
-        let (P.ProjectId id_) = P.id_ project
-        (li . (a ! href ("/projects/" <> toValue id_)) . toHtml . P.name) project
+        (li . (a ! href (toValue project)) . toHtml . P.name) project
 
 show_ :: P.Project -> Html
 show_ project = Layouts.app $ do
-    let (P.ProjectId id_) = P.id_ project
     H.div ! class_ "container" $ do
         H.div ! class_ "row" $ do
             H.div ! class_ "six columns" $ do
@@ -32,9 +30,9 @@ show_ project = Layouts.app $ do
                     li . toHtml $ "Name: " <> P.name project
                     li . toHtml $ "Description: " <> P.description project
         H.div ! class_ "row" $ do
-            H.form ! action ("/projects/" <> toValue id_) ! method "POST" $ do
+            H.form ! action (toValue project) ! method "POST" $ do
                 input ! type_ "hidden" ! name "_method" ! value "DELETE"
-                (a ! class_ "button" ! href ("/projects/" <> toValue id_ <> "/edit")) $ "Edit"
+                (a ! class_ "button" ! href (toValue project <> "/edit")) $ "Edit"
                 input ! type_ "submit" ! value "Delete"
         H.div ! class_ "row" $ do
             H.div ! class_ "six columns" $ do
@@ -57,10 +55,8 @@ new = Layouts.app $ do
 
 edit :: P.Project -> Html
 edit project = Layouts.app $ do
-    let (P.ProjectId id_) = P.id_ project
-    let url = "/projects/" <> toValue id_
     H.div ! class_ "container" $ do
-        H.form ! action url ! method "POST" $ do
+        H.form ! action (toValue project) ! method "POST" $ do
             input ! type_ "hidden" ! name "_method" ! value "PUT"
             H.div ! class_ "row" $ do
                 H.div ! class_ "six columns" $ do
@@ -70,5 +66,5 @@ edit project = Layouts.app $ do
                     H.label ! for "description" $ toHtml ("Description: " :: String)
                     input ! class_ "u-full-width" ! type_ "text" ! name "description" ! value (toValue . P.description $ project)
             H.div ! class_ "row" $ do
-                a ! class_ "button"! href url $ "Cancel"
+                a ! class_ "button"! href (toValue project) $ "Cancel"
                 input ! class_ "button-primary" ! type_ "submit" ! value "Submit"
